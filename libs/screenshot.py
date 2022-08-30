14624872a896984bda11ff2735e3017d3fc15ef2
Mathias Lindström <kuzeyron@gmail.com>
2022-08-30 23:41:38 +0300

    Tweaks

--- libs/screenshot.py
+++ libs/screenshot.py
@@ -5,8 +5,8 @@ from kivy.core.image import Image as CoreImage
 from kivy.lang import Builder
 from kivy.properties import BooleanProperty, ListProperty, NumericProperty
 from kivy.uix.image import Image
-from PIL import Image as CI
-from Xlib import X, display
+from Xlib.display import Display
+from PIL import ImageGrab
 
 __all__ = ('shoot', 'Shoot')
 
@@ -28,14 +28,8 @@ Builder.load_string('''
 
 def shoot(*largs):
     cached_screenshot = BytesIO()
-    disp = display.Display()
-    screen = disp.screen()
-    size = screen["width_in_pixels"], screen["height_in_pixels"]
-    raw = screen.root.get_image(0, 0, *size, X.ZPixmap, 0xffffff)
-    img = CI.frombytes("RGB", size, raw.data, "raw", "BGRX")
+    img = ImageGrab.grab(all_screens=True)
     img.save(cached_screenshot, format='png')
-    pos = screen.root.query_pointer()._data
-    cursor_pos = pos['root_x'], pos['root_y']
     cached_screenshot.seek(0)
 
     texture = CoreImage(
@@ -43,12 +37,12 @@ def shoot(*largs):
         ext='png'
     ).texture
 
-    return texture, cursor_pos
+    return texture
 
 
 class Shoot(Image):
     cursor_pos = ListProperty((0, 0))
-    cursor_size = NumericProperty('30dp')
+    cursor_size = NumericProperty('20dp')
     show_cursor = BooleanProperty(True)
 
     def __init__(self, **kwargs):
@@ -57,21 +51,28 @@ class Shoot(Image):
 
     @mainthread
     def frame(self, *largs):
-        self.texture, pos = shoot(self.size)
+        self.texture = shoot(self.size)
 
         if self.show_cursor:
-            self.cursor_position(pos)
+            Clock.schedule_once(self.cursor_position, 0)
 
         Clock.schedule_once(self.frame, 0)
 
     def cursor_position(self, pos, *largs):
         """ Calculate position from python-xlib
             and translate into Widget size """
+
+        disp = Display()
+        screen = disp.screen()
+
+        pos = screen.root.query_pointer()._data
+        pos = pos['root_x'], pos['root_y']
+
         tsize = self.texture_size
         norm = self.norm_image_size
-        x = pos[0] / tsize[0] * norm[0] - 4
+        x = pos[0] / tsize[0] * norm[0]
         y = pos[1] / tsize[1] * norm[1]
-        invert_y = self.height - y + 4
+        invert_y = self.height - y
 
         self.cursor_pos = x, invert_y - self.cursor_size
 
